ee6b87192b1e6c011c173806718984c3bd1c3149
amyhenning <henning.amy@gmail.com>
2018-06-22 01:28:19 +0000

    Added simple_form gem and set up form to add new hikes to the database. Updated some design things.

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -2,4 +2,24 @@ class HikesController < ApplicationController
 	def index
 		@hikes = Hike.all
 	end
+
+	def new
+		@hike = Hike.new
+	end
+
+	def create
+		@hike = Hike.create(hike_params)
+		redirect_to hike_path(@hike)
+	end
+
+	def show
+		@hike = Hike.find(params[:id])
+	end
+
+	private
+
+	def hike_params
+		params.require(:hike).permit(:name, :date, :description)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 	root 'hikes#index'
+	resources :hikes
 end
